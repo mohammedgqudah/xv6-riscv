2e1b136c822656a181e84b2166ca90d3e2cfd732
Mohammed AlQudah <mohghq@gmail.com>
2025-08-15 16:23:36 +0300

    use bindgen

--- rust/xv6/src/lib.rs
+++ rust/xv6/src/lib.rs
@@ -1,31 +1,26 @@
-#![no_std]
 //! Rust bindings for xv6 kernel functions.
 //!
 //! Instead of manually defining these, I should use something like bindgen.
 
+#![no_std]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+
 use core::panic::PanicInfo;
-unsafe extern "C" {
-    // Allocate one 4096-byte page of physical memory.
-    // Returns a pointer that the kernel can use.
-    // Returns 0 if the memory cannot be allocated.
-    unsafe fn kalloc() -> *mut core::ffi::c_void;
-    // Free the page of physical memory pointed at by pa,
-    // which normally should have been returned by a
-    // call to kalloc().  (The exception is when
-    // initializing the allocator; see kinit above.)
-    unsafe fn kfree(pa: *mut core::ffi::c_void);
-    unsafe fn printf(fmt: *const u8, ...) -> i32;
-    safe fn panic(msg: *const u8) -> !;
-}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn rs_hello() {
     unsafe {
-        printf(c"yay!\n".as_ptr());
+        printf(c"yay!\n".as_ptr() as *mut _);
     }
 }
 
 #[panic_handler]
 fn panic_handler(_info: &PanicInfo) -> ! {
-    panic(c"rust panic".as_ptr());
+    unsafe {
+        panic(c"rust panic".as_ptr() as *mut _);
+    }
 }
