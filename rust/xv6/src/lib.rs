0ffe25c5bc034d5890b616e5f2fde75f9501a8e7
Mohammed AlQudah <mohghq@gmail.com>
2025-08-15 16:50:19 +0300

    move bindings to xv6/bindings.rs

--- rust/xv6/src/lib.rs
+++ rust/xv6/src/lib.rs
@@ -3,24 +3,13 @@
 //! Instead of manually defining these, I should use something like bindgen.
 
 #![no_std]
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
 
 use core::panic::PanicInfo;
-
-#[unsafe(no_mangle)]
-pub extern "C" fn rs_hello() {
-    unsafe {
-        printf(c"yay!\n".as_ptr() as *mut _);
-    }
-}
+pub mod bindings;
 
 #[panic_handler]
 fn panic_handler(_info: &PanicInfo) -> ! {
     unsafe {
-        panic(c"rust panic".as_ptr() as *mut _);
+        bindings::panic(c"rust panic".as_ptr() as *mut _);
     }
 }
